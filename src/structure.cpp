4e55de29149f3f8eef755077d1a576adf6704ac5
Unknown <theo.delettre@gmail.com>
2019-04-21 00:39:37 +0200

    Added the base Automaton structures

--- src/structure.cpp
+++ src/structure.cpp
@@ -1,1 +1,12 @@
 #include "structure.h"
+
+FA::FA(std::string FAname)
+{
+    name= FAname;
+    determinized=minimized=complete= false;
+}
+
+FA::FA() {
+    name="Finite Automata";
+    determinized=minimized=complete= false;
+}
--- src/structure.h
+++ src/structure.h
@@ -1,9 +1,43 @@
 #ifndef FINITE_AUTOMATA_MATH_CS_STRUCTURE_H
 #define FINITE_AUTOMATA_MATH_CS_STRUCTURE_H
 
+#include <vector>
+
+// Outgoing transition from a State
+typedef struct Transition
+{
+    struct State * dest;
+    char trans;
+}Transition;
+
+//Single automata State
+typedef struct State
+{
+    //int id;       //Arbitrary state name          Currently this is automatic but we could have custom names
+    bool initial;   // true if this is an initial state
+    bool final;     // true if this is a final state
+    std::vector<Transition> exits;      //list of outgoing transitions
+}State;
 
 
+class FA
+{
+private:
+    std::string name;           //Arbitrary name
+    std::vector<State> states;  //List of all the automaton's states
 
+    //Shows if in its current state the automaton is minimized/determinized/complete
+    bool minimized;
+    bool determinized;
+    bool completed;
 
+public:
+    FA();
+    FA(std::string);
+    
 
+    bool complete();
+    FA minimize();
+    FA determinize();
+};
 #endif //FINITE_AUTOMATA_MATH_CS_STRUCTURE_H
